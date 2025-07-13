585ac5095aa50a2769fedc80d3a44e60b0903ed8
Khoa Phan <your.email@example.com>
2025-07-13 11:44:02 -0400

    gi coc

--- api/chat.js
+++ api/chat.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
-import dbConnect from '../../lib/dbConnect';
-import Message from '../../models/Message';
+import dbConnect from '../../lib/dbConnect.js';
+import Message from '../../models/Message.js';
 
 /**
  * Read and parse the raw request body as JSON.
