1602998ad161b5e4532ffc4c11b891c3324f8921
Khoa Phan <your.email@example.com>
2025-07-13 10:36:39 -0400

    Fix history.js

--- api/history.js
+++ api/history.js
@@ -1,5 +1,5 @@
 // pages/api/history.js
-import dbConnect from '../../lib/dbConnect';
+import dbConnect from '../../lib/dbConnect.js';
 import Message from '../../models/Message';
 
 export default async function handler(req, res) {
