5432441dde518dfd4b3154dc563d938371f52d75
Khoa Phan <your.email@example.com>
2025-07-16 09:06:00 -0400

    500 error code on Admin

--- api/analytics/summary.js
+++ api/analytics/summary.js
@@ -1,5 +1,5 @@
-import dbConnect from './lib/dbConnect.js';
-import Message from './models/Message.js';
+import dbConnect from '../lib/dbConnect.js';
+import Message from '../models/Message.js';
 
 export default async function handler(req, res) {
   await dbConnect();
